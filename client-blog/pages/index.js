86e1a120ab58278821270c5442e3e55a376552be
raidentek <33987212+raidentek@users.noreply.github.com>
2020-11-09 15:37:38 -0500

    updated

--- client-blog/pages/index.js
+++ client-blog/pages/index.js
@@ -10,6 +10,11 @@ export default function Home() {
         <h1 className="text-5xl text-center text-accent-1 ">
           Next.js + Tailwind CSS
         </h1>
+        <button className='btn-blue' >hola</button>
+
+        <div className='card-rol bg-indigo-800' >
+            <h4>peru</h4>
+        </div>
       </div>
       </main>
     </div>
